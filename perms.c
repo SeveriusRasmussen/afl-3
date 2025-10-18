fc768efec3cc178caeb833b4f9d3b327fc404dba
SeveriusRasmussen <sora0001@stud.kea.dk>
2025-10-18 20:11:51 +0200

    Noget gik galt, så jeg kopieret fra min anden projekt. Pga. glemt fork.

--- perms.c
+++ perms.c
@@ -3,26 +3,61 @@
 #include <stddef.h>
 
 // TODO: Den her funktion virker kun for en utrolig begrænset mængde input (u-r)
-//
+// Fra starten virker det kun for o-r.
 // Den skulle også gerne virke for:
 // - u, g, o
 // - + og -
 // - r, w, x, rw, rx, wx, rwx (eller en anden rækkefølge af dem)
 bool chmod(const char *input, struct perms_t *perms) {
-    if (input == NULL) {
+    if (!input || !perms) return false;
+
+    //1. Læs input:
+    char subj = input[0]; // u / g / o / a
+    char op = input[1]; // + eller -
+    if ((subj != 'u' && subj != 'g' && subj != 'o' && subj != 'a') ||
+        (op != '+' && op != '-'))
+    {
         return false;
     }
+    if (input[2] == '\0') return false;
 
-    if (input[0] != 'o') {
-        return false;
+    // 2. Byg masken
+    uint32_t mask = 0;
+    for (size_t i = 2; input[i] != '\0'; i++)
+    {
+        char r = input[i];
+
+        if (r == 'r')
+        {
+            if (subj == 'u' || subj == 'a') mask |= USER_READ;
+            if (subj == 'g' || subj == 'a') mask |= GROUP_READ;
+            if (subj == 'o' || subj == 'a') mask |= OTHER_READ;
+        }
+        else if (r == 'w')
+        {
+            if (subj == 'u' || subj == 'a') mask |= USER_WRITE;
+            if (subj == 'g' || subj == 'a') mask |= GROUP_WRITE;
+            if (subj == 'o' || subj == 'a') mask |= OTHER_WRITE;
+        }
+        else if (r == 'x')
+        {
+            if (subj == 'u' || subj == 'a') mask |= USER_EXECUTE;
+            if (subj == 'g' || subj == 'a') mask |= GROUP_EXECUTE;
+            if (subj == 'o' || subj == 'a') mask |= OTHER_EXECUTE;
+        }
+        else
+        {
+            return false; //ukendt tegn
+        }
     }
 
-    if (input[1] != '-') {
-        return false;
-    }
-
-    if (input[2] != 'r') {
-        return false;
+    // 3. Brug operatoren
+    if (op == '+')
+    {
+        perms->bits |= mask; //tænd bits
+    } else
+    {
+        perms->bits &= ~mask;
     }
 
     // Når man AND'er med en maske med 0'er i, slukker man for enkelte bits
@@ -34,6 +69,6 @@ bool chmod(const char *input, struct perms_t *perms) {
     // Eksempel:
     // 0644 og o-r giver 0640
     // og når man &'er med den maske, bliver den mindste bit sat til 0, og de resterende bits forbliver som de er.
-    perms->bits &= ~OTHER_READ;
+    // perms->bits &= ~OTHER_READ;
     return true;
 }
